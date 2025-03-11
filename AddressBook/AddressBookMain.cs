826d9bb5b81e2dde6960c44f37caffb9b957705c
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-11 17:22:57 +0530

    Removed unnecessary files

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hi! Welcome To Address Book System..!");
-            ContactPerson contact = new ContactPerson("Garvit","Chugh","gavi.chugh@gmail.com","Chd","Chd","Chd",160047,6283636757);
+            ContactPerson contact = new ContactPerson("Garvit", "Chugh", "gavi.chugh@gmail.com", "Chd", "Chd", "Chd", 160047, 6283636757);
             Console.WriteLine("Contact Created Successfully..!");
         }
     }
