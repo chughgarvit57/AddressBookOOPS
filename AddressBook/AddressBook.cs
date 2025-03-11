d03c5a55865348f8df9e4dcf885f016e2b61bed6
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-11 17:14:08 +0530

    Implemented UC1: Create Contact

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBook
+{
+    internal class AddressBook
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! Welcome To Address Book System..!");
+            ContactPerson contact = new ContactPerson("Garvit","Chugh","gavi.chugh@gmail.com","Chd","Chd","Chd",160047,6283636757);
+            Console.WriteLine("Contact Created Successfully..!");
+        }
+    }
+}
---
+++ AddressBook/ContactPerson.cs
@@ -0,0 +1,26 @@
+﻿namespace AddressBook
+{
+    public class ContactPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public long PhoneNumber { get; set; }
+
+        public ContactPerson(string firstName, string lastName, string email, string address, string city, string state, int zip, long phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
