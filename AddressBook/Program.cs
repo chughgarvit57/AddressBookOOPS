f735100df80ddfd3b81e1f123838d063b2e401aa
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-11 17:13:07 +0530

    Revert "Implemented UC1: Create Contact"
    This reverts commit 664f9213e38017bd07489584adecac57b4fdfbd9.

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,8 +5,6 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hi! Welcome To Address Book System..!");
-            ContactPerson contact = new ContactPerson("Garvit","Chugh","gavi.chugh@gmail.com","Chd","Chd","Chd",160047,6283636757);
-            Console.WriteLine(contact);
         }
     }
 }
