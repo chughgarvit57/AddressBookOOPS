664f9213e38017bd07489584adecac57b4fdfbd9
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-11 16:44:33 +0530

    Implemented UC1: Create Contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hi! Welcome To Address Book System..!");
+            ContactPerson contact = new ContactPerson("Garvit","Chugh","gavi.chugh@gmail.com","Chd","Chd","Chd",160047,6283636757);
+            Console.WriteLine(contact);
         }
     }
 }
