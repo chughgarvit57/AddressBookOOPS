ffd3659941225ec7ae8e4486f791ed4a6ba36f4d
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-11 18:36:21 +0530

    Minor fix in AddressBookMain class

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -9,7 +9,7 @@
             Console.WriteLine("Hi! Welcome To Address Book System..!");
             ContactPerson contact = new ContactPerson("Garvit", "Chugh", "gavi.chugh@gmail.com", "Chd", "Chd", "Chd", 160047, 6283636757);
             Console.WriteLine("Contact Created Successfully..!");
-            Console.WriteLine(contact);
+            Console.WriteLine(contact); 
         }
     }
 }
